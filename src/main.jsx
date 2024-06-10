1592ef9fe6fad52913b43c4360e372b650548889
Euclides Romero <euclidesantonioromeroibarra@gmail.com>
2024-06-10 01:10:01 -0500

    Despejando app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { AuthProvider } from './context/AuthProvider.jsx'
-import router from './Routes/Routes.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-     <RouterProvider router={router} />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 )
