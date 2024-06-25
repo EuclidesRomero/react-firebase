8172818f4c55656cd1388f9a349da09c269de4d7
Euclides Romero <euclidesantonioromeroibarra@gmail.com>
2024-06-25 13:43:06 -0500

    1.Se añadió una función para consumir los productos desde firestore, 2.Se usó async y await para los serivicios de firebase

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,46 +1,76 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
-import {  onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import {collection, getDocs, getFirestore} from 'firebase/firestore'
+import { onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import auth from '../credenciales'
 
 
+
 const AuthContext = createContext();
-
-
 const AuthProvider = ({ children }) => {
+    const db = getFirestore();
+
     const [usuario, setUsuario] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [productos, setProductos] = useState([])
 
-    const createUser = (email,password) => {
+
+    const createUser = (email, password) => {
         setLoading(true);
-        return createUserWithEmailAndPassword(auth, email,password );
+        try {
+            return createUserWithEmailAndPassword(auth, email, password);
+        } finally {
+            setLoading(false)
+        }
     }
 
-    const loginUser = (email, password) => {
-        setLoading(true);
-        return signInWithEmailAndPassword(auth, email, password)
+    const loginUser = async (email, password) => {
+        await signInWithEmailAndPassword(auth, email, password)
     }
 
-    const logOut = () => {
+    const logOut = async () => {
+        await signOut(auth);
         setLoading(true);
-        return signOut(auth);
-      };
+    };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (usuarioFirebase) => {
             if (usuarioFirebase) {
                 setUsuario(usuarioFirebase);
+                setLoading(false);
             } else {
                 setUsuario(null);
             }
         })
         return () => unsubscribe();
-    }, [])
+    }, []);
+
+    if(usuario ){
+        
+    }
+    useEffect(() => {
+        const fetchProducts = async () => {
+            if (loading) return;
+            else {
+                try {
+                    const querySnapshot = await getDocs(collection(db, "Productos"));
+                    const arrProductos = [];
+                    querySnapshot.forEach((doc) => {
+                        arrProductos.push({ id: doc.id, ...doc.data() });
+                    });
+                    setProductos(arrProductos);
+
+                } catch (error) {
+                    console.error('Error');
+                }
+            }
+        };
+        fetchProducts();
+    }, [loading]);
 
 
-    
     return (
-        <AuthContext.Provider value={{ usuario, createUser, loginUser, logOut, loading}}>
+        <AuthContext.Provider value={{ usuario, createUser, loginUser, logOut, loading, setLoading, productos }}>
             {children}
         </AuthContext.Provider>
     )
